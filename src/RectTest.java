e2027d5c0d52ad34cf62427e9c318c604e82640d
absolute88 <absolute964688@gmail.com>
2025-05-20 12:17:52 +0900

    MyFrame

---
+++ src/RectTest.java
@@ -0,0 +1,8 @@
+
+public class RectTest extends MyFrame{
+		// TODO 自動生成されたメソッド・スタブ
+		public void run(){
+			fillRect(30,50,200,100);
+		}
+
+}
--- src/start.java
+++ src/start.java
@@ -4,6 +4,7 @@ public class start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("aaaaa");
+		MyFrame frame1=new RectTest();
 	}
 
 }
