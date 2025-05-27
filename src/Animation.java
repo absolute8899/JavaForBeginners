8c13823ca2d669b90f486dbfae5347ccf440cf7c
absolute88 <absolute964688@gmail.com>
2025-05-27 11:44:50 +0900

    5-10

--- src/Animation.java
+++ src/Animation.java
@@ -3,7 +3,7 @@ public class Animation extends MyFrame{
 	public void run() {
 		int y=100;
 		int x=30;
-		while(y<=200) {
+		while(x<=170) {
 			clear();
 			setColor(0,128,0);
 			fillRect(x,y,10,100);
