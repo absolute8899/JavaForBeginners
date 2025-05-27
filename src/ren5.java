6650452cdd2c46cd947eee24a98380bd162728af
absolute88 <absolute964688@gmail.com>
2025-05-27 12:16:24 +0900

    Animation 練習5の1~3

---
+++ src/ren5.java
@@ -0,0 +1,76 @@
+
+public class ren5 extends MyFrame{
+	public void run() {
+		
+//5-1
+//		int x=100;
+//		int y=200;
+//		while(y>=100) {
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			y-=5;
+//			sleep(0.1);
+//		}
+//		while(x<=200) {
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x+=5;
+//			sleep(0.1);
+//		}
+		
+		
+//5-2
+//		int x=50;
+//		int y=50;
+//		while(x<=300) {
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x+=7;
+//			y+=5;
+//			sleep(0.1);
+//		}
+//		while(x>=50) {
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x-=7;
+//			sleep(0.1);
+//		}
+//		while(x<=300) {
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x+=7;
+//			y-=5;
+//			sleep(0.1);
+//		}
+//		
+		
+
+//5-3
+//		int x=50;
+//		int y=50;
+//		int i;
+//		
+//		for(i=1;i<4;i++) {
+//		while(x<=170) {
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x+=5;
+//			sleep(0.1);
+//		}
+//		while(x>=30) {
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x-=5;
+//			sleep(0.1);
+//		}
+//		}
+	}
+
+}
--- src/start.java
+++ src/start.java
@@ -14,7 +14,8 @@ public class start {
 		//MyFrame frame8=new ren4noex();
 		//MyFrame frame9=new ren4no2();
 		//MyFrame frame10=new DisplayRectsSlow();
-		MyFrame frame11=new Animation();
+		//MyFrame frame11=new Animation();
+		MyFrame frame12=new ren5();
 	}
 
 }
