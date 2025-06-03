41044896d8774ba794f1665d3fc392bd087495dd
absolute88 <absolute964688@gmail.com>
2025-06-03 12:58:24 +0900

    車両速度変更

--- src/DisplayCarVector.java
+++ src/DisplayCarVector.java
@@ -3,13 +3,13 @@ import java.util.Vector;
 public class DisplayCarVector extends MyFrame{
 	public void run() {
 		Vector<car> cars=new Vector<car>();
-		cars.add(new car(50,50,1,0));
-		cars.add(new car(50,100,2,0));
-		cars.add(new car(50,150,3,0));
-		cars.add(new car(50,200,4,0));
-		cars.add(new car(50,250,5,0));
+		cars.add(new car(50,50,5,0));
+		cars.add(new car(50,100,6,0));
+		cars.add(new car(50,150,7,0));
+		cars.add(new car(50,200,8,0));
+		cars.add(new car(50,250,9,0));
 		
-		for(int i=0;i<100;i++) {
+		for(int i=0;i<70;i++) {
 			clear();
 			
 			for(int j=0;j<cars.size();j++) {
